b5a3356a1afb384d7ad800606593b44899bca83f
Robyn <Robyn796@mac.com>
2024-07-17 16:58:42 -0600

    w2update

---
+++ database/assignment2.sql
@@ -0,0 +1,5 @@
+INSERT INTO account (first_name, last_name, email, password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+UPDATE account
+SET account_type = 'Admin'
+WHERE email = 'tony@starkent.com';
