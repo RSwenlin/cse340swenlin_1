2bd95864cb85abb91b30ceda45dfbf8dda2a4b31
Robyn <Robyn796@mac.com>
2024-07-19 15:44:47 -0600

    error part 2

--- utilities/index.js
+++ utilities/index.js
@@ -25,7 +25,7 @@ Util.getNav = async function (req, res, next) {
 } 
 
 
-module.exports = Util
+
 
 /* **************************************
 * Build the classification view HTML
@@ -71,3 +71,15 @@ Util.buildClassificationGrid = async function(data){
         </div>
     `;
 };
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+
+
+
+module.exports = Util
