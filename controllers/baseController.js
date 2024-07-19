54b26a64388dec7a86465bebc00c721e894b4032
Robyn <Robyn796@mac.com>
2024-07-19 16:21:51 -0600

    error 2

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,6 +1,9 @@
 const utilities = require("../utilities/")
 const baseController = {}
-
+/*  ******************************
+ *  Build Home view with MVC
+ * Unit 3, Activities
+ *  ****************************** */
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
   res.render("index", {title: "Home", nav})
