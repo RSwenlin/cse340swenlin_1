36fab3044929e6bec8f6c03df71046306fd1bfab
Robyn <Robyn796@mac.com>
2024-07-19 15:08:03 -0600

    more backtrack

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,10 +4,12 @@ const router = new express.Router();
 const invController = require("../controllers/invController");
 const validate = require("../utilities/inventory-validation");
 const utilities = require('../utilities');
+const invChecks = require("../utilities/inventory-validation")
 
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+// unit 3 activities
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 module.exports = router;
 
