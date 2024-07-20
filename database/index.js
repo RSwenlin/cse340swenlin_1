150ca5962511e21c7d70dba51dd3898acff83f6e
Robyn <Robyn796@mac.com>
2024-07-19 18:50:32 -0600

    why not

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
-const { Pool } = require("pg")
+/*const { Pool } = require("pg")
 require("dotenv").config()
 /* *******************
 * Connection Pool
@@ -6,7 +6,7 @@ require("dotenv").config()
 *  But will cause problems in production environment
 * If-else will make determination which to use
 * ********************  */
-let pool
+/*let pool
 if (process.env.NODE_ENV == "development") {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
@@ -35,4 +35,48 @@ if (process.env.NODE_ENV == "development") {
     })
     
 }
-module.exports = pool
+module.exports = pool */
+const { Pool } = require("pg");
+require("dotenv").config();
+
+/* *******************
+* Connection Pool
+* SSL Object needed for local testing of app
+* But will cause problems in production environment
+* If-else will make determination which to use
+* ********************  */
+let pool;
+if (process.env.NODE_ENV === "development") {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+
+    // Added for troubleshooting queries during development
+    const query = async (text, params) => {
+        try {
+            const res = await pool.query(text, params);
+            console.log("executed query", { text });
+            return res;
+        } catch (error) {
+            console.error("error in query", { text });
+            throw error;
+        }
+    };
+
+    module.exports = {
+        query,
+        pool, // Export pool for other uses
+    };
+} else {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+    });
+
+    module.exports = {
+        query: (text, params) => pool.query(text, params), // Use the same query interface
+        pool,
+    };
+}
