9a1b832e6f1b42be83139f819f1caafa516f6904
Robyn <Robyn796@mac.com>
2024-07-22 19:51:03 -0600

    inv route fix

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -9,15 +9,13 @@ const invChecks = require("../utilities/inventory-validation")
 // Route to build inventory by classification view
 // unit 3 activities
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
-
-module.exports = router; 
-
+ 
 //*const express = require('express');
 //*const router = express.Router();
 //*const inventoryController = require('../controllers/inventoryController');
 
 // Define a route to handle specific inventory item detail view
-router.get('/detail/:id', inventoryController.getInventoryDetail);
+router.get('/detail/:id', invController.getInventoryDetail);
 
 // Route to display delete confirmation view
 router.get("/delete/:inv_id", utilities.checkLogin, utilities.checkAdmin, utilities.handleErrors(invController.buildDeleteConfirmationView));
